efe69087e537f36733988acd5c71db885e2d931e
Nate Florendo <nateodnerolf@gmail.com>
2022-12-10 02:07:25 +0800

    - Added a Round/wave System (Nate Florendo)

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 //
 //  Copyright © 2022 Kyo Matias, Nate Florendo. All rights reserved.
@@ -9,14 +12,20 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private float _spawnRadius = 5f;
-    [SerializeField] private float _time = 1.5f;
+    private float _timeInterval = 3.2f;
+    private float _subtractedTimeIntervalPerRound = 0.15f;
+    private float _minTimeInterval = 0.20f;
+    private int _counter = 0;
+    private int _timePerRound = 30;
+    
 
     public GameObject[] enemies;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnEnemy());
+        StartCoroutine(RoundCounter());  //can be commented
     }
 
     IEnumerator SpawnEnemy()  //spawns enemy based on radius around player
@@ -25,7 +34,21 @@ public class EnemySpawner : MonoBehaviour
         spawnPos += Random.insideUnitCircle.normalized * _spawnRadius;
 
         Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPos, Quaternion.identity);
-        yield return new WaitForSeconds(_time);
+        yield return new WaitForSeconds(_timeInterval);
         StartCoroutine(SpawnEnemy());  //loops
+    }
+
+    IEnumerator RoundCounter()  // can be commented
+    {
+        _counter++;  //add counts per round
+        
+        Debug.Log($"Round: {_counter}");
+        
+        if (_timeInterval > _minTimeInterval) // prevents to make interval to 0
+        {
+            _timeInterval -= _subtractedTimeIntervalPerRound;  //subtracts interval time per round to make it harder
+        }
+        yield return new WaitForSeconds(_timePerRound);
+        StartCoroutine(RoundCounter()); 
     }
 }
