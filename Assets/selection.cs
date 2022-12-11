211c554164bbcdc84f7496f7a6aac4571207f557
Kyo Matias <kyomatias.acad@gmail.com>
2022-12-11 14:36:53 +0800

    Updated Sprites for Characters and Updated Main Menu Added

---
+++ Assets/selection.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+//  .cs
+//  Script
+//
+//  Created by Kyo Matias on 00/00/2022.
+//  Copyright © 2022 Kyo Matias. All rights reserved.
+//
+public class selection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
