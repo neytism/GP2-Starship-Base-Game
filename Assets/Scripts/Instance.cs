d9ed913eaa106975b7952d77c7a3dfc0886d710b
Nate Florendo <nateodnerolf@gmail.com>
2022-12-12 22:06:17 +0800

    - Added Instance Script

---
+++ Assets/Scripts/Instance.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instance : MonoBehaviour
+{
+    public GameObject myPrefab;
+
+    void Start()
+    {
+        Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
